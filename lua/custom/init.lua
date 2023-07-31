2c7b336ffff21522c959b78058803d9c2d8acdfe
Kabir Bissessar <kabir.bissessar@aalto.fi>
2023-07-31 10:53:43 +0300

    added custom to git

---
+++ lua/custom/init.lua
@@ -0,0 +1,16 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+local opt = vim.opt
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
